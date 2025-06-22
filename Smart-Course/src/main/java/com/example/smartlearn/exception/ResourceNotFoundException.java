369bccb3f242eec5ab0b3e95ff05bebd6503a30e
Jupiter-0717 <18697795717@163.com>
2025-06-22 23:27:38 +0800

    删除Smart-Course

--- Smart-Course/src/main/java/com/example/smartlearn/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartlearn.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
